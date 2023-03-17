f973b613b60898f5a1f8ee0f1cbdd1d25cfcef95
eraybulut34 <abdullaheraybulut@hotmail.com>
2023-03-17 18:55:22 +0300

    left menu

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,53 @@
 import React from "react";
-import { Table } from "antd";
+import { Col, Row, List } from "antd";
+const entryTitles = [
+  {
+    title: "bugün bunlar oldu",
+    link: "/",
+  },
+  {
+    title: "bugün bunlar oldu",
+    link: "/",
+  },
+  {
+    title: "bugün bunlar oldu",
+    link: "/",
+  },
+  {
+    title: "bugün bunlar oldu",
+    link: "/",
+  }]
+
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Link",
+      dataIndex: "link",
+      key: "link",
+    },
+  ];
+
+  const contentStyle: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    height: "100vh",
+  };
 
 function HomePage() {
   return (
-    <div className="w-screen h-screen">
-      anasayfa
+    <div>
+      <Row style={contentStyle}>
+        {/* left menu with antd*/}
+        <Col span={4} style={{ backgroundColor: "grey",padding: "1rem" }}>
+          <List dataSource={entryTitles} renderItem={(item) => <List.Item>{item.title}</List.Item>} />
+        </Col>
+        <Col span={18}></Col>
+      </Row>
+
     </div>
   );
 }
