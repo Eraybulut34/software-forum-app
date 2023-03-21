9760e64b79f82cabee711f84e4f6301f7dbd249a
eraybulut34 <abdullaheraybulut@hotmail.com>
2023-03-21 14:03:23 +0300

    deploy fix

--- src/components/content/ContentCard.tsx
+++ src/components/content/ContentCard.tsx
@@ -8,8 +8,8 @@ const cardStyle = {
     boxSizing: "border-box",
     borderRadius: "8px",
     boxShadow: "0 1px 2px 0 rgba(0,0,0,0.2), 0 3px 5px 0 rgba(0,0,0,0.19)"
-}
-
+} as React.CSSProperties;
+ 
 
 function ContentCard(props: any) {
   const { title, description, image } = props;
